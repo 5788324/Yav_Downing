ddc79247438a3a95aed7b32b3a6b22f4641b0c78
5788324 <121728123+5788324@users.noreply.github.com>
2026-07-27 12:00:06 +0800

    release: set Yav version to 2.0.0

--- backend/runtime.py
+++ backend/runtime.py
@@ -13,7 +13,7 @@ from pathlib import Path
 from urllib.error import URLError
 from urllib.request import urlopen
 
-APP_VERSION = "2.0.0-rc5"
+APP_VERSION = "2.0.0"
 
 
 def default_data_dir() -> Path:
